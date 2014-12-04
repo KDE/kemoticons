ef27cc057c07550e9d9b5b503554a4c61d207ff3
Dan Vrátil <dvratil@redhat.com>
2014-12-04 10:59:26 +0100

    Add KEmoticonsIntegrationPlugin for KTextToHTML from KCoreAddons
    KTextToHTML is a class to convert plaintext into HTML, including linkifying
    URLs, email addresses, markup, and can also convert text smileys into
    <img...> images. For the last it requires KEmoticons. Since KCoreAddons cannot
    depend on KEmoticons, we have KEmoticons install a run-time integration plugin,
    that KTextToHTML will load when available. Without this plugin, the simileys
    conversion is simply skipped.
    
    REVIEW: 121118

---
+++ autotests/ktexttohtmlplugintest.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2014  Daniel Vrátil <dvratil@redhat.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <QObject>
+#include <QTest>
+#include <QStandardPaths>
+
+#include "../src/integrationplugin/ktexttohtml.h"
+#include <kemoticons.h>
+
+class KTextToHTMLPluginTest : public QObject
+{
+    Q_OBJECT
+
+private:
+    QString htmlForSmiley(const QString &emoticon, const QString &name) const;
+
+private Q_SLOTS:
+    void initTestCase();
+    void parseEmoticonsTest_data();
+    void parseEmoticonsTest();
+
+private:
+    QString mEmoticonsThemePath;
+};
+
+QTEST_MAIN(KTextToHTMLPluginTest)
+
+void KTextToHTMLPluginTest::initTestCase()
+{
+    mEmoticonsThemePath = QStandardPaths::locate(QStandardPaths::GenericDataLocation, "emoticons/Glass",  QStandardPaths::LocateDirectory);
+    QVERIFY(!mEmoticonsThemePath.isEmpty());
+}
+
+QString KTextToHTMLPluginTest::htmlForSmiley(const QString &emoticon, const QString &name) const
+{
+    return QString("<img align=\"center\" title=\"%1\" alt=\"%1\" src=\"%2/%3.png\" width=\"22\" height=\"22\" />")
+                .arg(emoticon)
+                .arg(mEmoticonsThemePath)
+                .arg(name);
+}
+
+
+void KTextToHTMLPluginTest::parseEmoticonsTest_data()
+{
+
+    QTest::addColumn<QString>("input");
+    QTest::addColumn<bool>("strict");
+    QTest::addColumn<QStringList>("exclude");
+    QTest::addColumn<QString>("expected");
+
+    QTest::newRow("simple")
+        << "Hello :-)"
+        << false << QStringList()
+        << QString("Hello %1").arg(htmlForSmiley(":-)", "smile"));
+
+    QTest::newRow("between strings")
+        << "Hello :-) How are you?"
+        << false << QStringList()
+        << QString("Hello %1 How are you?").arg(htmlForSmiley(":-)", "smile"));
+
+    QTest::newRow("excluded")
+        << "Bye :-("
+        << false << (QStringList() << ":-(")
+        << "Bye :-(";
+
+    QTest::newRow("don't mix in HTML")
+        << "<b>:(</b>"
+        << false << QStringList()
+        << QString("<b>%1</b>").arg(htmlForSmiley(":(", "sad"));
+
+    QTest::newRow("strict parsing of smileys without space")
+        << "Very happy! :-):-)"
+        << true << QStringList()
+        << "Very happy! :-):-)";
+
+    QTest::newRow("nonstrict parsing of smileys without space")
+        << "Very happy! :-):-)"
+        << false << QStringList()
+        << QString("Very happy! %1%1").arg(htmlForSmiley(":-)", "smile"));
+
+    QTest::newRow("smiley in HTML")
+        << "<img src=\"...\" title=\":-)\" />"
+        << false << QStringList()
+        << "<img src=\"...\" title=\":-)\" />";
+}
+
+
+void KTextToHTMLPluginTest::parseEmoticonsTest()
+{
+    QFETCH(QString, input);
+    QFETCH(bool, strict);
+    QFETCH(QStringList, exclude);
+    QFETCH(QString, expected);
+
+    KTextToHTMLEmoticons emoticons;
+    const QString actual = emoticons.parseEmoticons(input, strict, exclude);
+
+    QCOMPARE(actual, expected);
+}
+
+#include "ktexttohtmlplugintest.moc"
---
+++ src/integrationplugin/ktexttohtml.cpp
@@ -0,0 +1,40 @@
+/*  This file is part of the KDE libraries
+ *  Copyright 2014 Daniel Vrátil <dvratil@redhat.com>
+ *
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License or ( at
+ *  your option ) version 3 or, at the discretion of KDE e.V. ( which shall
+ *  act as a proxy as in section 14 of the GPLv3 ), any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+#include "ktexttohtml.h"
+
+#include <KEmoticons/KEmoticons>
+#include <KEmoticons/KEmoticonsTheme>
+
+KTextToHTMLEmoticons::KTextToHTMLEmoticons()
+{
+}
+
+QString KTextToHTMLEmoticons::parseEmoticons(const QString &text,
+                                             bool strictParse,
+                                             const QStringList &exclude)
+{
+    KEmoticonsTheme theme = KEmoticons().theme();
+    KEmoticonsTheme::ParseMode mode = KEmoticonsTheme::DefaultParse;
+    if (strictParse) {
+        mode = KEmoticonsTheme::StrictParse;
+    }
+    return theme.parseEmoticons(text, mode, exclude);
+}
